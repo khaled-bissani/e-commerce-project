f1feca9733b473117e12d35aa05f3ab644ed5dc0
Khaled Saleh <saleh_khaled.97@outlook.com>
2022-09-21 17:54:09 +0300

    starting JS, controlling the log in div

--- client-frontend/scripts/landing-frontend.js
+++ client-frontend/scripts/landing-frontend.js
@@ -1,1 +1,16 @@
-console.log("test01");
+const login_nav = document.getElementById("login-btn");
+const signup_nav = document.getElementById("signup-btn");
+
+const login_span = document.getElementById("login-span");
+const signup_span = document.getElementById("signup-span");
+
+login_nav.addEventListener("click", showLogin);
+
+function showLogin(){
+    const signin_div = document.getElementsByClassName("signin-div");
+    const signup_div = document.getElementsByClassName("signup-form-div");
+    signup_div[0].style.display = "none";
+    console.log(signin_div);
+    console.log(signup_div);
+}
+
