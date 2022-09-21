86281e5aeb5210586401599751abd6598d929740
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-21 20:51:56 +0300

    Adding-sellerform-editsellerform

--- admin-electron/scripts/admin-sellerform-frontend.js
+++ admin-electron/scripts/admin-sellerform-frontend.js
@@ -2,18 +2,35 @@
 const main=document.querySelector('#main');
 const open_add=document.querySelector('#open-add');
 const close_add=document.querySelector('#close-add');
+const open_edit=document.querySelector('#open-edit');
+const close_edit=document.querySelector('#close-edit');
 const add_seller=document.querySelector('#add-seller');
+const edit_seller=document.querySelector('#edit-seller');
 
-
+//open addseller form
 open_add.addEventListener('click',()=>{
     add_seller.style.display='block';
     main.classList.add('disable');    
 })
 
+//close addseller form
 close_add.addEventListener('click',()=>{
     add_seller.style.display='none';
     main.classList.remove('disable');
 })
+
+//open editseller form
+open_edit.addEventListener('click',()=>{
+    edit_seller.style.display='block';
+    main.classList.add('disable');    
+})
+
+//close addseller form
+close_edit.addEventListener('click',()=>{
+    edit_seller.style.display='none';
+    main.classList.remove('disable');
+})
+
 
 //connect the table
 const table_seller=document.querySelector('#table-seller');
