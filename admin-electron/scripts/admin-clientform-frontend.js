aa87224c214801e9f8fe1da9be56d68d47d42653
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-21 15:43:29 +0300

    Adding-clientsform-tableclient

---
+++ admin-electron/scripts/admin-clientform-frontend.js
@@ -0,0 +1,16 @@
+//connect the table
+let table=document.querySelector('#table');
+
+//create a new row
+for(let i=0;i<5;i++){ 
+    let tempalte=`
+    <tr>
+        <td><img class='table-image' src=''></td>
+        <td><p class='table-info'></p></td>
+        <td><p class='table-info'></p></td>
+        <td><p class='table-info'></p></td>
+        <td><p class='table-info'></p></td>
+    </tr>`;
+
+    table.innerHTML+=tempalte;
+}
