6cdae79d634c37eb9607f80644de97acb9ddc1f8
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-21 16:09:21 +0300

    Making the header section fixed as a sub navbar

--- client-frontend/scripts/home-frontend.js
+++ client-frontend/scripts/home-frontend.js
@@ -1,1 +1,13 @@
-// Khaled Bissani
+// Execute the function on scroll
+window.onscroll = function() {onScroll()};
+
+let subNavbar = document.getElementById("sub-nav");
+let fixed = subNavbar.offsetTop;
+
+function onScroll() {
+  if (window.pageYOffset >= fixed) {
+    subNavbar.classList.add("fixed")
+  } else {
+    subNavbar.classList.remove("fixed");
+  }
+}
