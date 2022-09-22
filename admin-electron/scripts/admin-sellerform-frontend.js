229f5a5104d99ca36e7ecd28812669d8a0ead107
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-22 16:59:28 +0300

    Connecting-sellerform-tagstojavascript

--- admin-electron/scripts/admin-sellerform-frontend.js
+++ admin-electron/scripts/admin-sellerform-frontend.js
@@ -7,6 +7,18 @@ const close_edit=document.querySelector('#close-edit');
 const add_seller=document.querySelector('#add-seller');
 const edit_seller=document.querySelector('#edit-seller');
 
+const seller_id=document.querySelector('#seller-id');
+const add=document.querySelector('#add');
+const edit=document.querySelector('#edit');
+const delete_button=document.querySelector('#delete');
+
+const seller_name=document.querySelector('#sellername');
+const seller_username=document.querySelector('#sellerusername');
+const seller_email=document.querySelector('#selleremail');
+
+const edit_name=document.querySelector('#editname');
+const edit_email=document.querySelector('#editemail');
+
 //open addseller form
 open_add.addEventListener('click',()=>{
     add_seller.style.display='block';
@@ -40,13 +52,20 @@ for(let i=0;i<15;i++){
     let tempalte=`
     <tr>
         <td><img class='table-image' src=''></td>
-        <td><p class='table-info'></p></td>
-        <td><p class='table-info'></p></td>
-        <td><p class='table-info'></p></td>
-        <td><p class='table-info'></p></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
     </tr>`;
 
     table_seller.innerHTML+=tempalte;
 }
 
+//get the ID of each client by clicking on the row
+for(var i=1; i < table_seller.rows.length; i++){
+    table_seller.rows[i].onclick = function(){
+    seller_id.value=this.cells[1].innerHTML;
+    delete_button.disabled=false;
+    }
+}
 
