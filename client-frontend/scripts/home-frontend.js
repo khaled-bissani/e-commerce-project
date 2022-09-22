6f81f002ac953638ef4418dcbf04e22cf17504a6
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-22 09:49:43 +0300

    Setting up the cart popup

--- client-frontend/scripts/home-frontend.js
+++ client-frontend/scripts/home-frontend.js
@@ -11,15 +11,25 @@ function onScroll() {
     subNavbar.classList.remove("fixed");
   }
 }
-
 // Display the voucher popup on click
 const voucherPopup = document.getElementById('voucher-popup');
 const voucherBtn = document.getElementById('voucher-btn');
-const closeVoucher = document.getElementById('close');
+const closeVoucher = document.getElementById('close-voucher');
 
 voucherBtn.onclick = function() {
     voucherPopup.style.display = "block";
 }
 closeVoucher.onclick = function() {
     voucherPopup.style.display = "none";
+}
+// Display the voucher popup on click
+const cartPopup = document.getElementById('cart-popup');
+const cartBtn = document.getElementById('cart-btn');
+const closeCart = document.getElementById('close-cart');
+
+cartBtn.onclick = function() {
+    cartPopup.style.display = "block";
+}
+closeCart.onclick = function() {
+    cartPopup.style.display = "none";
 }
