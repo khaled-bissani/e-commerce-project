372c4c467082b781ace6a7dbd45d44bf7a3f9b8d
hicham <hicham.hachem1@gmail.com>
2022-09-22 12:18:06 +0300

    Creating revenue section

--- seller-frontend/scripts/seller-frontend.js
+++ seller-frontend/scripts/seller-frontend.js
@@ -2,30 +2,43 @@
 const sec_home = document.getElementById("home-section");
 const sec_category = document.getElementById("category-section");
 const sec_revenue = document.getElementById("revenue-section");
+const sec_discount = document.getElementById("discount-section");
 
 const btn_home = document.getElementById("home");
 const btn_category = document.getElementById("category");
 const btn_revenue = document.getElementById("revenue");
+const btn_discount = document.getElementById("discount");
 
 // view home-section
 btn_home.addEventListener('click',()=>{
     sec_home.style.display='block';
     sec_category.style.display='none';
-    sec_revenue.style.display='none';    
+    sec_revenue.style.display='none';   
+    sec_discount.style.display='none';  
 })
 
 // view category-section
 btn_category.addEventListener('click',()=>{
     sec_home.style.display='none';
     sec_category.style.display='block';
-    sec_revenue.style.display='none';    
+    sec_revenue.style.display='none'; 
+    sec_discount.style.display='none';     
 })
 
 // view revenue-section
 btn_revenue.addEventListener('click',()=>{
     sec_home.style.display='none';
     sec_category.style.display='none';
-    sec_revenue.style.display='block';    
+    sec_revenue.style.display='block';   
+    sec_discount.style.display='none';   
+})
+
+// view discount-section
+btn_discount.addEventListener('click',()=>{
+    sec_home.style.display='none';
+    sec_category.style.display='none';
+    sec_revenue.style.display='none';   
+    sec_discount.style.display='block';   
 })
 
 ////////////////////////////////////////////////////
