e6752e1de248d7acf4e22d8b5955717c0ad30746
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-24 10:50:58 +0300

    Getting-numberofclient

--- admin-electron/scripts/number_client.js
+++ admin-electron/scripts/number_client.js
@@ -2,7 +2,7 @@
 const number_client=document.querySelector('#numberclient');
 
 //get number of client
-axios.get('http://localhost/e-commerce-project/ecommerce-server/admin-backend/number_client.php').then((response) => {
+axios.get('http://localhost/ecommerce-server/admin-backend/number_client.php').then((response) => {
   console.log('api',response.data.number_of_client);
   number_client.innerHTML=response.data.number_of_client;
   }).catch((error)=> {
