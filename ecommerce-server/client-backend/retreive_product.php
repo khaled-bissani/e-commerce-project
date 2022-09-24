c05752953834db62e4ee96dab594171c0d99fb6d
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-24 10:19:54 +0300

    Adding the api to get all the product to the home page

---
+++ ecommerce-server/client-backend/retreive_product.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+include("connection.php");
+$query = $mysqli->prepare("SELECT name, price FROM products");
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
+​?>
