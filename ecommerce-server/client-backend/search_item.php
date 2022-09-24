a883af898102d99b05edddf15f177ad10d928d25
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-24 15:45:01 +0300

    Adding headers for all api

--- ecommerce-server/client-backend/search_item.php
+++ ecommerce-server/client-backend/search_item.php
@@ -1,6 +1,8 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
 include("connection.php");
 $searched_item = $_POST['searched_item'];
