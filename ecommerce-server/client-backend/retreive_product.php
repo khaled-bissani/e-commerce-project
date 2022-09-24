f679d21cd9ad40670b7719f42f7a1122385cf466
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-24 21:17:07 +0300

    Adding image attribute to the retreive product query

--- ecommerce-server/client-backend/retreive_product.php
+++ ecommerce-server/client-backend/retreive_product.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
 include("connection.php");
-$query = $mysqli->prepare("SELECT name, price FROM products");
+$query = $mysqli->prepare("SELECT name, price, image FROM products");
 $query->execute();
 $array = $query->get_result();
 $response = [];
@@ -15,4 +15,4 @@ while($a = $array->fetch_assoc()){
 
 $json = json_encode($response);
 echo $json;
-​?>
+?>
