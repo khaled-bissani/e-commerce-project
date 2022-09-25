01f9bead1854737fe6a86f8032f2b7b3bafc21c9
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-25 18:39:18 +0300

    Getting-totalrevenue

--- seller-frontend/scripts/seller-revenue-frontend.js
+++ seller-frontend/scripts/seller-revenue-frontend.js
@@ -2,12 +2,20 @@
 const userID = localStorage.getItem('data');
 
 //connect HTML tag
-const title_revenue=document.querySelector('#total-revenue');
+const total_revenue=document.querySelector('#total-revenue');
 const week=document.querySelector('#value-week');
 const month=document.querySelector('#value-month');
 const year=document.querySelector('#value-year');
 
 const revenue_product=document.querySelector('#revenue-product');
+
+//getting total revenue
+const revenueData = new FormData();
+revenueData.append('id',userID);
+axios.post('http://localhost/ecommerce-server/seller-backend/revenue.php',revenueData)
+.then(res => 
+total_revenue.innerHTML=`Total Revenue ${res.data.revenue}$`)
+.catch(err=>console.log(err));
 
 
 for(let i=0;i<5;i++){
