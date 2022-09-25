1e4740426acf9e2d8f401419575334c55f1dd23a
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-25 17:31:51 +0300

    Adding api to give the id of eachh seller

---
+++ ecommerce-server/client-backend/get_seller_id.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+include("connection.php");
+
+$name=$_POST['name'];
+$query = $mysqli->prepare("SELECT id FROM users WHERE name=? and types_id=2");
+$query->bind_param("s", $name);
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($a = $array->fetch_assoc()){
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
+?>
